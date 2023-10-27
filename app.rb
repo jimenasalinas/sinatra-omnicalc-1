feba7f44c58026d5e2b867c319cc4f7e0ff62e3d
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-10-27 23:04:06 +0000

    working on payment format

--- app.rb
+++ app.rb
@@ -13,6 +13,10 @@ get("/square_root/new") do
   erb(:new_square_root)
 end
 
+get("/payment/new") do
+  erb(:new_payment)
+end
+
 get("/square/results") do
   @the_num = params.fetch("users_number").to_f
 
@@ -27,10 +31,22 @@ get("/square_root/results") do
   erb(:square_root_results)
 end
 
+get("/payment/results") do
+  @apr = params.fetch("users_apr").to_f
+
+  @years = params.fetch("users_years").to_i
+
+  @principal = params.fetch("users_principal").to_f
+
+  monthly_interest_rate = (@apr / 12) / 100
+  total_payments = @years * 12
+
+  @payment = (monthly_interest_rate * @principal) / (1 - (1 + monthly_interest_rate)**(-total_payments)).to_s_currency
+
+  erb(:payment_results)
+end
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:new_square_calc)
 end
