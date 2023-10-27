995e2848caf04408be87774e14b33d345a7c3139
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-10-27 15:45:05 +0000

    starting app

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  "Hello there"
+  erb(:hello)
+end
+
+get("/goodbye") do
+  Goodbye
+
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
