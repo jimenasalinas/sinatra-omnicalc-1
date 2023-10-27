268e142cd60070006eeff9c61de65c46e0f3854d
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-10-27 21:54:10 +0000

    first tab working

--- app.rb
+++ app.rb
@@ -2,17 +2,18 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/howdy") do
-  "Hello there"
   erb(:hello)
-end
-
-get("/goodbye") do
-  Goodbye
-
 end
 
 get("/square/new") do
   erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+  erb(:square_results)
 end
 
 get("/") do
