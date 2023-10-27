94346b223628cdfb0782a92bbcf4efef276579c3
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-10-27 22:21:08 +0000

    editing square root

--- app.rb
+++ app.rb
@@ -9,11 +9,22 @@ get("/square/new") do
   erb(:new_square_calc)
 end
 
+get("/square_root/new") do
+  erb(:new_square_root)
+end
+
 get("/square/results") do
   @the_num = params.fetch("users_number").to_f
 
   @the_result = @the_num ** 2
   erb(:square_results)
+end
+
+get("/square_root/results") do
+  @the_num_r = params.fetch("users_root_number").to_f
+
+  @the_result_r = @the_num_r.to_f ** (1/2)
+  erb(:square_root_results)
 end
 
 get("/") do
